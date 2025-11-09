93ab51c7e29b8db48d86cdb6a7fbf853311661bb
transflo <ubitx@outlook.com>
2025-11-10 02:45:52 +0800

    Add files via upload

--- create-socat-service.sh
+++ create-socat-service.sh
@@ -180,3 +180,146 @@ for LISTEN_PORT in "${PORTS[@]}"; do
     echo ""
 done
 
+# 配置系统内核参数
+echo -e "${YELLOW}配置系统内核参数...${NC}"
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.bk_$(date +%Y%m%d_%H%M%S) && sudo sh -c 'echo "kernel.pid_max = 65535
+
+kernel.panic = 1
+
+kernel.sysrq = 1
+
+kernel.core_pattern = core_%e
+
+kernel.printk = 3 4 1 3
+
+kernel.numa_balancing = 0
+
+kernel.sched_autogroup_enabled = 0
+
+
+
+vm.swappiness = 10
+
+vm.dirty_ratio = 10
+
+vm.dirty_background_ratio = 5
+
+vm.panic_on_oom = 1
+
+vm.overcommit_memory = 1
+
+vm.min_free_kbytes = 39014
+
+
+
+net.core.default_qdisc = cake
+
+net.core.netdev_max_backlog = 2000
+
+net.core.rmem_max = 8388608
+
+net.core.wmem_max = 8388608
+
+net.core.rmem_default = 87380
+
+net.core.wmem_default = 65536
+
+net.core.somaxconn = 256
+
+net.core.optmem_max = 65536
+
+
+
+net.ipv4.tcp_fastopen = 3
+
+net.ipv4.tcp_timestamps = 1
+
+net.ipv4.tcp_tw_reuse = 1
+
+net.ipv4.tcp_fin_timeout = 10
+
+net.ipv4.tcp_slow_start_after_idle = 0
+
+net.ipv4.tcp_max_tw_buckets = 32768
+
+net.ipv4.tcp_sack = 1
+
+net.ipv4.tcp_fack = 0
+
+
+
+net.ipv4.tcp_rmem = 8192 87380 3939361
+
+net.ipv4.tcp_wmem = 8192 65536 1969680
+
+net.ipv4.tcp_mtu_probing = 1
+
+net.ipv4.tcp_congestion_control = bbr
+
+net.ipv4.tcp_notsent_lowat = 4096
+
+net.ipv4.tcp_window_scaling = 1
+
+net.ipv4.tcp_adv_win_scale = 4
+
+net.ipv4.tcp_moderate_rcvbuf = 1
+
+net.ipv4.tcp_no_metrics_save = 0
+
+
+
+net.ipv4.tcp_max_syn_backlog = 2048
+
+net.ipv4.tcp_max_orphans = 65536
+
+net.ipv4.tcp_synack_retries = 2
+
+net.ipv4.tcp_syn_retries = 3
+
+net.ipv4.tcp_abort_on_overflow = 0
+
+net.ipv4.tcp_stdurg = 0
+
+net.ipv4.tcp_rfc1337 = 0
+
+net.ipv4.tcp_syncookies = 1
+
+
+
+net.ipv4.ip_local_port_range = 1024 65535
+
+net.ipv4.ip_no_pmtu_disc = 0
+
+net.ipv4.route.gc_timeout = 100
+
+net.ipv4.neigh.default.gc_stale_time = 120
+
+net.ipv4.neigh.default.gc_thresh3 = 8192
+
+net.ipv4.neigh.default.gc_thresh2 = 4096
+
+net.ipv4.neigh.default.gc_thresh1 = 1024
+
+
+
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+
+net.ipv4.conf.all.rp_filter = 1
+
+net.ipv4.conf.default.rp_filter = 1
+
+net.ipv4.conf.all.arp_announce = 2
+
+net.ipv4.conf.default.arp_announce = 2
+
+net.ipv4.conf.all.arp_ignore = 1
+
+net.ipv4.conf.default.arp_ignore = 1" > /etc/sysctl.conf' && sudo sysctl -p
+
+if [ $? -eq 0 ]; then
+    echo -e "${GREEN}✓ 系统内核参数配置完成${NC}"
+else
+    echo -e "${RED}✗ 系统内核参数配置失败${NC}"
+fi
